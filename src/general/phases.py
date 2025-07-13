c42080cfa36431796201084a936ff09cd4f20f2c
Hellixoid <Hellixoid@gmail.com>
2025-07-13 04:29:37 +0300

    Support for P1 bis lists for wowtbc

--- src/general/phases.py
+++ src/general/phases.py
@@ -2,19 +2,19 @@ phases = {
     "Pre-Bis": 0,
     # "T11": 1,
     # "T12": 1
-    "T13": 1
+    "T14": 1
 }
 
 id_to_phase = {
     0: "Pre-Bis",
     # 1: "T11",
     # 1: "T12",
-    1: "T13"
+    1: "T14"
 }
 
 phase_to_name = {
     "Pre-Bis": "PR",
     # "T11": "P1",
     # "T12": "P3"
-    "T13": "P4"
+    "T14": "P1"
 }
