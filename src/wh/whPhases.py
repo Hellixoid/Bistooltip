79cd34b9e9272700350b167cfe926b161fee2305
Hellixoid <Hellixoid@gmail.com>
2025-07-29 03:10:23 +0300

    Fix hunter subphase order (wowhead)

--- src/wh/whPhases.py
+++ src/wh/whPhases.py
@@ -8,11 +8,11 @@ phase_gear_panel_sorting = {
         # pre
         "tab-mop-phase-1-pre-bis-pre-raid-opening",
         "tab-mop-phase-1-pre-raid-opening",
-        "tab-bis-lists-pre-raid-bis",
         "tab-mop-phase-1-pre-bis-phase-1-pre-bis",
         "tab-bis-lists-pre-raid-opening",
         "tab-mop-phase-1-realistic-pre-raid-opening",
         "tab-mop-phase-1-pre-bis-pre-raid-opening-pre-bis"
+        "tab-bis-lists-pre-raid-opening-bis",
         # toes
         "tab-bis-lists-toes-vendor-release",
         "tab-bis-lists-toes-release",
@@ -23,21 +23,21 @@ phase_gear_panel_sorting = {
         "tab-bis-lists-pre-raid-hof-celestial",
         "tab-bis-lists-hof-vendor-release",
         # celest msv
+        "tab-bis-lists-pre-raid-bis",
         "tab-bis-lists-msv-vendor-release",
         "tab-mop-phase-1-pre-bis-msv-celestial-pre-bis",
         "tab-bis-lists-msv-release",
         "tab-mop-phase-1-msv-release",
-        "tab-bis-lists-pre-raid-opening-bis",
         "tab-bis-lists-pre-raid-msv-celestial",
         "tab-mop-phase-1-pre-bis-celestial-msv-release-pre-bis",
     ],
     1: [
-        # start
-        "tab-bis-lists-mogushan-vaults",
         # complete
         "tab-bis-lists-phase-1-bis",
         "tab-bis-lists-phase-1-bis-tierless",
         # hof
         "tab-bis-lists-heart-of-fear",
+        # start
+        "tab-bis-lists-mogushan-vaults",
     ]
 }
